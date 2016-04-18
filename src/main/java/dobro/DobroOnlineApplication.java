ac8c9a6f0fd21848257ee386886d600526a2c878
originalWho <da.erow@gmail.com>
2016-04-18 16:52:00 +0300

    Created project

---
+++ src/main/java/dobro/DobroOnlineApplication.java
@@ -0,0 +1,12 @@
+package dobro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobroOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobroOnlineApplication.class, args);
+	}
+}
