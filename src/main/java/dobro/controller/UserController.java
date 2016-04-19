33786a5a82a97feee42960e19d99e082b739db42
originalWho <da.erow@gmail.com>
2016-04-19 12:40:30 +0300

    HomeController deleted, its methods are moved to UserController; form validation of quick register implemented

--- src/main/java/dobro/controller/UserController.java
+++ src/main/java/dobro/controller/UserController.java
@@ -1,8 +1,14 @@
 package dobro.controller;
 
+import dobro.model.User;
 import dobro.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by Artur on 4/18/16.
@@ -14,5 +20,20 @@ public class UserController {
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(User user){
+        return "home";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String checkForm(@Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.out.println("Form has errors");
+            return "home";
+        }
+        userService.saveUser(user);
+        return "redirect:/";
     }
 }
